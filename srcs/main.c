057a87242573f6525cca09d34ede74385ff191ea
DimiOui <omygoditsdimi@gmail.com>
2022-02-09 15:35:11 +0100

    removed ugly tester/changed space management

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,27 @@
 /*   By: dimioui <dimioui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:37:52 by dimioui           #+#    #+#             */
-/*   Updated: 2022/02/04 15:30:11 by dimioui          ###   ########.fr       */
+/*   Updated: 2022/02/09 15:34:04 by dimioui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
+
+void	ft_delspace(char *str)
+{
+	int	write;
+	int	read;
+
+	write = 0;
+	read = 0;
+	while(str[read])
+	{
+		if (str[read] != ' ')
+			str[write++] = str[read];
+		read++;
+	}
+	str[write] = 0;
+}
 
 void	ft_algo_choice(t_list *stack_a, t_list *stack_b)
 {
@@ -38,13 +54,9 @@ int	ft_parsing(int ac, char **av)
 	j = 0;
 	if (ac < 2)
 		return (0);
-	//if (ac == 2)
-	//{
-	//	ft_putendl_fd("Error", 2);
-	//	return (0);
-	//}
 	while (i < ac)
 	{
+		ft_delspace(av[i]);
 		if (!ft_check_nb(j, av[i]) || !ft_check_space(av[i])
 			|| !ft_check_sign(av[i]) || !ft_check_double(ac, av))
 		{
@@ -75,19 +87,3 @@ int	main(int ac, char **av)
 	ft_lstclear_swap(&stack_b);
 	return (0);
 }
-
-	//while (stack_a)
-	//{
-	//	printf("%d\n", stack_a->content);
-	//	stack_a = stack_a->next;
-	//}
-// now need to do the algorithm
-// algochoice function will decide which sorting algorithm we'll chose
-// for size == 2 -> we do 'sa'
-// for size == 3 -> we'll try to implement a simple algo
-// for size == 4 -> same as above
-// size == 5 -> same as above
-// size == 100 -> insertion sort
-// else, radx sort
-
-// TEST WITH A LONG OVERFLOW FOR INT LIMITS
